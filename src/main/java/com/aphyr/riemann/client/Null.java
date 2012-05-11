3f708524fe3da6a36d49405b825864eab52dce3e
Edward Ribeiro <edward.ribeiro@gmail.com>
2012-05-10 22:36:57 -0300

    Preventing Null from being instantiated.

--- src/main/java/com/aphyr/riemann/client/Null.java
+++ src/main/java/com/aphyr/riemann/client/Null.java
@@ -3,4 +3,5 @@ package com.aphyr.riemann.client;
 // null has a type, but you can't use it! Since the JVM doesn't have Maybe[], we'll just make one up instead.
 // The *only* acceptable argument for f(Null n) is f(null).
 public class Null {
+	private Null() {}
 }
