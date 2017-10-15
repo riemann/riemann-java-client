9c19a1908e6454a2abd92a9eafffd02db70cf5f1
James Turnbull <james@lovedthanlost.net>
2017-10-15 10:34:10 +0100

    Tidy travis file

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -ex
+set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
   tar -xzvf protobuf-2.6.1.tar.gz
