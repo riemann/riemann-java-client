c55afec4fd5785609e9a6104fdbc2b93b166386e
James Turnbull <james@lovedthanlost.net>
2022-01-08 15:53:09 -0500

    Updated protobuf to 3.16.1 for https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-22569
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- scripts/install-protobuf.sh
+++ scripts/install-protobuf.sh
@@ -2,8 +2,8 @@
 set -ex
 if [ ! -f "$HOME/bin/protoc" ]; then
   cd $HOME
-  wget https://github.com/google/protobuf/releases/download/v3.9.1/protoc-3.9.1-linux-x86_64.zip
-  unzip protoc-3.9.1-linux-x86_64.zip
+  wget https://github.com/google/protobuf/releases/download/v3.16.1/protoc-3.16.1-linux-x86_64.zip
+  unzip protoc-3.16.1-linux-x86_64.zip
 else
   echo "Using cached directory."
 fi
